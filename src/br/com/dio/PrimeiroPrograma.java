aa7a9e576d1af58f7ace326645099e16917ee6e4
Leonardo <leonardoss615@gmail.com>
2022-03-31 23:19:16 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -10,6 +10,7 @@ public class PrimeiroPrograma {
 
         /*int a = 5;
         int b = 3;
-        System.out.println("Hello World " + (a+b));*/
+        System.out.println("Hello World " + (a+b));
+        comment */
     }
 }
